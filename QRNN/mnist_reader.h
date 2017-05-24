81b8310acddc9430016537b330a0a065ce944328
MedyG <guohx3@hotmail.com>
2017-05-24 23:59:18 +0800

    styles

--- QRNN/mnist_reader.h
+++ QRNN/mnist_reader.h
@@ -16,8 +16,7 @@ using namespace std;
 
 namespace mnist {
 
-	int ReverseInt(int i)
-	{
+	int ReverseInt(int i) {
 		unsigned char ch1, ch2, ch3, ch4;
 		ch1 = i & 255;
 		ch2 = (i >> 8) & 255;
@@ -26,11 +25,9 @@ namespace mnist {
 		return((int)ch1 << 24) + ((int)ch2 << 16) + ((int)ch3 << 8) + ch4;
 	}
 
-	void read_mnist_images(string filename, vector<vector<double>> &vec, int number_of_images, int n_rows, int n_cols)
-	{
+	void read_mnist_images(string filename, vector<vector<double>> &vec, int number_of_images, int n_rows, int n_cols) {
 		ifstream file(filename, ios::binary);
-		if (file.is_open())
-		{
+		if (file.is_open()) {
 			int magic_number = 0x803;
 			file.read((char*)&magic_number, sizeof(magic_number));
 			magic_number = ReverseInt(magic_number);
@@ -40,13 +37,10 @@ namespace mnist {
 			n_rows = ReverseInt(n_rows);
 			file.read((char*)&n_cols, sizeof(n_cols));
 			n_cols = ReverseInt(n_cols);
-			for (int i = 0; i < number_of_images; ++i)
-			{
+			for (int i = 0; i < number_of_images; ++i) {
 				vector<double> tp;
-				for (int r = 0; r < n_rows; ++r)
-				{
-					for (int c = 0; c < n_cols; ++c)
-					{
+				for (int r = 0; r < n_rows; ++r) {
+					for (int c = 0; c < n_cols; ++c) {
 						unsigned char temp = 0;
 						file.read((char*)&temp, sizeof(temp));
 						tp.push_back((double)temp);
@@ -57,11 +51,9 @@ namespace mnist {
 		}
 	}
 
-	void read_Mnist_Label(string filename, vector<double> &vec)
-	{
+	void read_Mnist_Label(string filename, vector<double> &vec) {
 		ifstream file(filename, ios::binary);
-		if (file.is_open())
-		{
+		if (file.is_open()) {
 			int magic_number = 0;
 			int number_of_images = 0;
 			int n_rows = 0;
@@ -70,8 +62,7 @@ namespace mnist {
 			magic_number = ReverseInt(magic_number);
 			file.read((char*)&number_of_images, sizeof(number_of_images));
 			number_of_images = ReverseInt(number_of_images);
-			for (int i = 0; i < number_of_images; ++i)
-			{
+			for (int i = 0; i < number_of_images; ++i) {
 				unsigned char temp = 0;
 				file.read((char*)&temp, sizeof(temp));
 				vec[i] = (double)temp;
